01351636556e4f21d508e7cfc51ba5571b398b7d
Moisés Peláez <moises.jpelaez@gmail.com>
2026-05-08 20:49:30 -0400

    Add one shot logic nodes

---
+++ armory/Sources/armory/logicnode/PlayOneShotActionNode.hx
@@ -0,0 +1,30 @@
+package armory.logicnode;
+
+import iron.object.Object;
+
+class PlayOneShotActionNode extends LogicNode {
+
+	public function new(tree: LogicTree) {
+		super(tree);
+	}
+
+	override function run(from: Int) {
+		var object: Object = inputs[1].get();
+		var action: String = inputs[2].get();
+		var blendTime: Float = inputs[3].get();
+		var speed: Float = inputs[4].get();
+		var loop: Bool = inputs[5].get();
+		var boneCollection: String = inputs[6].get();
+
+		if (object == null) return;
+		var animation = object.animation;
+		if (animation == null) animation = object.getParentArmature(object.name);
+		if (animation == null) return;
+
+		animation.playOneShot(action, function() {
+			runOutput(1);
+		}, blendTime, speed, loop, boneCollection);
+
+		runOutput(0);
+	}
+}
---
+++ armory/blender/arm/logicnode/animation/LN_play_one_shot_action.py
@@ -0,0 +1,33 @@
+from arm.logicnode.arm_nodes import *
+
+
+class PlayOneShotActionNode(ArmLogicTreeNode):
+    """
+    Plays an animation action as a one-shot over the given Bone Collection mask.
+
+    @input In: Activates the node logic.
+    @input Object: States object/armature to run the animation action on.
+    @input Action: States animation action to be played as a one-shot.
+    @input Blend: Reserved for one-shot blend time.
+    @input Speed: Sets rate the animation plays at.
+    @input Loop: Sets whether the one-shot should rewind itself after finishing.
+    @input Bone Collection: Limits the one-shot to bones in this Blender Bone Collection. Leave empty to affect all bones.
+
+    @output Out: Executes whenever the node is run.
+    @output Done: Executes whenever the one-shot animation is finished.
+    """
+    bl_idname = 'LNPlayOneShotActionNode'
+    bl_label = 'Play One Shot Action'
+    arm_version = 1
+
+    def arm_init(self, context):
+        self.add_input('ArmNodeSocketAction', 'In')
+        self.add_input('ArmNodeSocketObject', 'Object')
+        self.add_input('ArmNodeSocketAnimAction', 'Action')
+        self.add_input('ArmFloatSocket', 'Blend', default_value=0.0)
+        self.add_input('ArmFloatSocket', 'Speed', default_value=1.0)
+        self.add_input('ArmBoolSocket', 'Loop', default_value=False)
+        self.add_input('ArmStringSocket', 'Bone Collection')
+
+        self.add_output('ArmNodeSocketAction', 'Out')
+        self.add_output('ArmNodeSocketAction', 'Done')
